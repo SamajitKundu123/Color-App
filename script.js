6fcbffe14dde23b549e91057c3499732c46a065f
Samajit Kundu <126185958+SamajitKundu123@users.noreply.github.com>
2024-02-14 18:19:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const generaterBtn = document.getElementById("gneratorBtn");
+generaterBtn.addEventListener("click", updateColor)
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function updateColor(){
+
+    const colorBox = document.getElementById("colorBox");
+    const colorText = document.getElementById("colorBox");
+
+    const randomColor = getRandomColor();
+    colorBox.style.backgroundColor = randomColor;
+    colorText.textContent = randomColor;
+
+}
